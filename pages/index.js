f33f30247aecd9b37cacafb6f284e3ed6be135ec
jsdevrazu <islamrajulearn.dev@gmail.com>
2022-12-15 15:58:24 +0600

    added player api

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,12 @@
-import { useState } from 'react'
+import { useState } from "react";
 
 export default function Home() {
-  const [selectedPlayer , setSelectedPlayer] = useState(1)
-
-  const [playerOne] = useState("X")
-  const [playerTwo] = useState("O")
-  const [playerThree] = useState("Z")
+  const [selectedPlayer, setSelectedPlayer] = useState(1);
+  const [playerOne] = useState("X");
+  const [playerTwo] = useState("O");
+  const [playerThree] = useState("Z");
+  const [winner, setWinner] = useState(null);
+  const [playerName, setPlayerName] = useState("");
 
   const [board, setBoard] = useState([
     ["", "", "", "", "", "", ""],
@@ -15,37 +16,63 @@ export default function Home() {
     ["", "", "", "", "", "", ""],
     ["", "", "", "", "", "", ""],
     ["", "", "", "", "", "", ""],
-  ])
+  ]);
 
+  async function saveScore(winnerName) {
+    const postData = {
+      player1: playerOne,
+      player2: playerTwo,
+      player3: playerThree,
+      winner: winnerName === 1 && playerOne || winnerName === 2 && playerTwo || winnerName === 3 && playerThree,
+    };
+    try {
+      await fetch(`http://localhost:4000/api/v1/game-results`, {
+        method: "post",
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(postData),
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  async function getData() {
+    if(!playerName) return;
+    try {
+      const res = await fetch(
+        `http://localhost:4000/api/v1/game-results/${playerName}`
+      );
+      const data = await res.json();
+      setWinner(data);
+    } catch (error) {
+      console.log(error);
+    }
+  }
 
   const clickHandler = (mainIndex, index) => {
-     if (board[mainIndex][index] != "") {
-       return alert("Already selected!")
-     }else {
+    if (board[mainIndex][index] != "") {
+      return alert("Already selected!");
+    } else {
       setBoard((prev) => {
-        const updatedArr = [...prev]
+        const updatedArr = [...prev];
         updatedArr[mainIndex][index] =
           selectedPlayer == 1
             ? playerOne
             : selectedPlayer == 2
             ? playerTwo
-            : playerThree
+            : playerThree;
 
-        return updatedArr
-      })
+        return updatedArr;
+      });
 
-       if (winnerCheck(selectedPlayer, mainIndex, index)) {
-      alert(`Player ${selectedPlayer} wins!`);
-      resetBoard();
+      if (winnerCheck(selectedPlayer, mainIndex, index)) {
+        saveScore(selectedPlayer);
+        alert(`Player ${selectedPlayer} wins!`);
+        resetBoard();
+      }
+
+      setSelectedPlayer(selectedPlayer == 1 ? 2 : selectedPlayer == 2 ? 3 : 1);
     }
-
-
-
-      setSelectedPlayer(selectedPlayer == 1 ? 2 : selectedPlayer == 2 ? 3 : 1)
-
-     }
-    
-  }
+  };
 
   const winnerCheck = (selectedPlayer, mainIndex, index) => {
     const player =
@@ -53,27 +80,22 @@ export default function Home() {
         ? playerOne
         : selectedPlayer == 2
         ? playerTwo
-        : playerThree
-    
-    let count = 1
+        : playerThree;
+
+    let count = 1;
     for (let i = 0; i < 7; i++) {
-      if (board[mainIndex][i] === player) count++
-      else count = 1
-      if (count === 5) return true
+      if (board[mainIndex][i] === player) count++;
+      else count = 1;
+      if (count === 5) return true;
     }
 
-    count = 1
+    count = 1;
     for (let i = 0; i < 7; i++) {
-      if (board[i][index] === player) count++
-      else count = 1
-      if (count === 5) return true
+      if (board[i][index] === player) count++;
+      else count = 1;
+      if (count === 5) return true;
     }
-
-
-    
-
-  }
-
+  };
 
   const resetBoard = () => {
     setBoard([
@@ -84,9 +106,9 @@ export default function Home() {
       ["", "", "", "", "", "", ""],
       ["", "", "", "", "", "", ""],
       ["", "", "", "", "", "", ""],
-    ])
-  }
-  
+    ]);
+  };
+
   return (
     <>
       <h1 style={{ margin: 5, textAlign: "center" }}>
@@ -106,18 +128,43 @@ export default function Home() {
                 {val.map((value, inx) => {
                   return (
                     <div
-                    key={inx}
+                      key={inx}
                       onClick={() => clickHandler(mindex, inx)}
-                      style={{ height: 50, width: 50, border: "1px solid red", display : "flex", alignItems : "center", justifyContent : "center", backgroundColor : "#cccccc", cursor : "pointer" }}
-                    >{value}</div>
-                  )
+                      style={{
+                        height: 50,
+                        width: 50,
+                        border: "1px solid red",
+                        display: "flex",
+                        alignItems: "center",
+                        justifyContent: "center",
+                        backgroundColor: "#cccccc",
+                        cursor: "pointer",
+                      }}
+                    >
+                      {value}
+                    </div>
+                  );
                 })}
               </div>
             </div>
-          )
+          );
         })}
       </div>
-    </>
-  )
-}
 
+      <div>
+        <h1>Check Result List</h1>
+        <p>
+          Playername must be: {playerOne} | {playerTwo} | {playerThree}{" "}
+        </p>
+       {winner && <p>Mr {winner && playerName}: Win: {winner?.wins} | Lost: {winner?.losses}</p>} 
+        <input
+          type="text"
+          placeholder="Enter player name"
+          value={playerName}
+          onChange={(e) => setPlayerName(e.target.value)}
+        />
+        <button onClick={getData}>Get result</button>
+      </div>
+    </>
+  );
+}
